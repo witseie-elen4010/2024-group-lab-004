1800ed7f1f8e8ce324c2b3d5301abe9f758dcb2e
Rudolph Torlage <torlagerudi@gmail.com>
2024-04-18 17:54:46 +0200

    Initializing the application

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const dotenv = require('dotenv')
+
+dotenv.config({ path: './config.env' })
+const app = require('./app')
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+  console.log(`Server running on local port ${port}...`)
+})
