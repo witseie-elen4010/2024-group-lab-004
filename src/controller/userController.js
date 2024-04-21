0350a9818d93ce2a2799dd8b33f284fc5a929a4d
Rudolph Torlage <torlagerudi@gmail.com>
2024-04-21 20:06:08 +0200

    Added DB connection and account creation

---
+++ src/controller/userController.js
@@ -0,0 +1,31 @@
+const db = require('../db/database')
+
+exports.checkUserAccount = async (req, res) => {
+  const { username, password } = req.body
+  const query = 'SELECT * FROM users WHERE username = $1 AND password = $2'
+  const values = [username, password]
+  try {
+    const result = await db.query(query, values)
+
+    if (result.rowCount === 0) {
+      return res.redirect('/')
+    } else {
+      return res.redirect('/draw')
+    }
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
+}
+
+exports.createUserAccount = async (req, res) => {
+  const { username, password } = req.body
+  const query = 'INSERT INTO users (username, password) VALUES ($1, $2)'
+  const values = [username, password]
+
+  try {
+    const result = await db.query(query, values)
+    return res.redirect('/draw')
+  } catch (error) {
+    return res.status(500).json({ message: error.message })
+  }
+}
