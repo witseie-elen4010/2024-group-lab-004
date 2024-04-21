b1d08055b1f577c8fb1d4472a954e912393a018a
Rudolph Torlage <torlagerudi@gmail.com>
2024-04-21 20:32:35 +0200

    Added warning upon incorrect credentials

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const welcomeController = require('../controller/welcomeController')
+const userController = require('../controller/userController')
 const drawController = require('../controller/drawController')
 
 const router = express.Router()
 
-router.route('/').get(welcomeController.welcome)
+router.route('/').get(welcomeController.login)
 router.route('/draw').get(drawController.board)
-router.route('/:id').get()
-router.route('/').post()
+// router.route('/:id').get()
+router.route('/login').post(userController.checkUserAccount)
+router.route('/register').post(userController.createUserAccount)
 
 module.exports = router
