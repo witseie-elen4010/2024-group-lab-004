b86fa85f20a775c7c0b8f5593cf08a06aeccc9c1
Rudolph Torlage <torlagerudi@gmail.com>
2024-04-23 14:37:43 +0200

    added unit tests

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -1,29 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const urlParams = new URLSearchParams(window.location.search)
-  const loginError = urlParams.get('loginError')
-  const errorElement = document.getElementById('loginError')
+// Existing code for toggling forms
+const loginButton = document.getElementById('loginButton')
+const registerButton = document.getElementById('registerButton')
+const loginForm = document.getElementById('loginForm')
+const registerForm = document.getElementById('registerForm')
 
-  if (loginError) {
-    errorElement.style.display = 'block' // Show the error message if loginError is true
-  }
-
-  // Existing code for toggling forms
-  const loginButton = document.getElementById('loginButton')
-  const registerButton = document.getElementById('registerButton')
-  const loginForm = document.getElementById('loginForm')
-  const registerForm = document.getElementById('registerForm')
-
-  loginButton.addEventListener('click', function () {
-    loginForm.style.display = 'block'
-    registerForm.style.display = 'none'
-    loginButton.classList.add('active')
-    registerButton.classList.remove('active')
-  })
-
-  registerButton.addEventListener('click', function () {
-    registerForm.style.display = 'block'
-    loginForm.style.display = 'none'
-    registerButton.classList.add('active')
-    loginButton.classList.remove('active')
-  })
+registerButton.addEventListener('click', function () {
+  registerForm.style.display = 'block'
+  loginForm.style.display = 'none'
+  registerButton.classList.add('active')
+  loginButton.classList.remove('active')
 })
--- src/routes/router.js
+++ src/routes/router.js
@@ -8,7 +8,6 @@ const router = express.Router()
 router.route('/').get(welcomeController.login)
 router.route('/draw').get(drawController.board)
 // router.route('/:id').get()
-router.route('/login').post(userController.checkUserAccount)
 router.route('/register').post(userController.createUserAccount)
 
 module.exports = router
