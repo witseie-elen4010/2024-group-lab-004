34b7b6125d4d29a7eefcad63026b2f2248b2e6c5
Rudolph Torlage <torlagerudi@gmail.com>
2024-04-23 14:45:12 +0200

    standardized port to 4000

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config({ path: './config.env' })
 const app = require('./app')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.listen(port, () => {
   console.log(`Server running on local port ${port}...`)
