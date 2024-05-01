dedbb2b8bf47f035fa8adf9186e0999655f9ed2e
Rudolph Torlage <torlagerudi@gmail.com>
2024-05-01 18:33:26 +0200

    fixed db sometimes connecting to local server

--- src/db/database.js
+++ src/db/database.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  host: process.env.DB_HTTP,
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
@@ -10,13 +10,5 @@ const pool = new Pool({
 
 console.clear()
 console.log(`Database server running on port ${pool.options.port}...`)
-
-exports.query = async (text, params) => {
-  const start = Date.now()
-  const res = await pool.query(text, params)
-  const duration = Date.now() - start
-  console.log('executed query', { text, duration, rows: res.rowCount })
-  return res
-}
 
 module.exports = pool
