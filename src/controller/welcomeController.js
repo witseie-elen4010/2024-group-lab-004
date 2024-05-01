52266ef2466830d9c33c8291759fe31b6eb817c0
Rudolph Torlage <torlagerudi@gmail.com>
2024-05-01 17:23:39 +0200

    temporarily commented out redirects for users who arent logged in

--- src/controller/welcomeController.js
+++ src/controller/welcomeController.js
@@ -1,9 +1,5 @@
 const path = require('path')
 
-// exports.welcome = async (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/html', 'index.html'))
-// }
-
 exports.login = async (req, res) => {
   res.sendFile(path.join(__dirname, '../public/html', 'login.html'))
 }
@@ -12,12 +8,16 @@ exports.welcome = async (req, res) => {
   res.sendFile(path.join(__dirname, '../public/html', 'welcome.html'))
 }
 
+// exports.landing = async (req, res) => {
+//   if (req.session.user) {
+//     res.sendFile(
+//       path.join(__dirname, '..', './public/html', 'landingPage.html')
+//     )
+//   } else {
+//     res.redirect('/') // Redirect to login if no session is found
+//   }
+// }
+
 exports.landing = async (req, res) => {
-  if (req.session.user) {
-    res.sendFile(
-      path.join(__dirname, '..', './public/html', 'landingPage.html')
-    )
-  } else {
-    res.redirect('/') // Redirect to login if no session is found
-  }
+  res.sendFile(path.join(__dirname, '..', './public/html', 'landingPage.html'))
 }
