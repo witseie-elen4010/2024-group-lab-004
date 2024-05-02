03af4cedf2584ed5a4a9b6a2f0b66826ba0902a8
Rudolph Torlage <torlagerudi@gmail.com>
2024-05-02 11:01:12 +0200

    Removed faulty tests

--- tests/pastPrompts.spec.js
+++ tests/pastPrompts.spec.js
@@ -1,16 +1,6 @@
 import { test, expect } from '@playwright/test'
 import exp from 'constants'
 
-test('guest sees no past games', async ({ page }) => {
-  await page.goto('http://localhost:4000/')
-  await page.getByRole('button', { name: 'Continue as Guest' }).click()
-  await page.getByPlaceholder('Enter your nickname').click()
-  await page.getByPlaceholder('Enter your nickname').fill('test')
-  await page.getByRole('button', { name: 'Join' }).click()
-  await page.getByRole('button', { name: 'History' }).click()
-  expect(await page.locator('#gameList').innerText().toBe(''))
-})
-
 test('user sees past games', async ({ page }) => {
   await page.goto('http://localhost:4000/')
   await page.getByRole('button', { name: 'Sign In' }).click()
@@ -23,8 +13,9 @@ test('user sees past games', async ({ page }) => {
     .getByRole('button', { name: 'Login' })
     .click()
   await page.getByRole('button', { name: 'History' }).click()
-  await page.waitForTimeout(5000)
-  expect(await page.locator('#gameList').innerText().not.toBe(''))
+  await page.getByRole('button', { name: 'Next' }).click()
+  await page.getByRole('button', { name: 'Previous' }).click()
+  expect(await page.getByText('test1 - 4/30/').isVisible())
 })
 
 test('next page button works', async ({ page }) => {
@@ -39,10 +30,8 @@ test('next page button works', async ({ page }) => {
     .getByRole('button', { name: 'Login' })
     .click()
   await page.getByRole('button', { name: 'History' }).click()
-  await page.waitForTimeout(5000)
-  expect(await page.getByRole('button', { name: 'Next' }).toBeVisible())
   await page.getByRole('button', { name: 'Next' }).click()
-  expect(await page.locator('#gameList').innerText().not.toBe(''))
+  expect(await page.getByText('test11 - 4/30/').isVisible())
 })
 
 test('previous page button works', async ({ page }) => {
@@ -57,41 +46,7 @@ test('previous page button works', async ({ page }) => {
     .getByRole('button', { name: 'Login' })
     .click()
   await page.getByRole('button', { name: 'History' }).click()
-  await page.waitForTimeout(5000)
-  expect(await page.getByRole('button', { name: 'Previous' }).not.toBeVisible())
   await page.getByRole('button', { name: 'Next' }).click()
-  await page.waitForTimeout(500)
-  expect(await page.getByRole('button', { name: 'Previous' }).toBeVisible())
-})
-
-test('user with no past games sees nothing', async ({ page }) => {
-  await page.goto('http://localhost:4000/')
-  await page.getByRole('button', { name: 'Sign In' }).click()
-  await page.getByLabel('Username:').click()
-  await page.getByLabel('Username:').fill('rudi')
-  await page.getByLabel('Password:').click()
-  await page.getByLabel('Password:').fill('me')
-  await page
-    .locator('#loginForm')
-    .getByRole('button', { name: 'Login' })
-    .click()
-  await page.getByRole('button', { name: 'History' }).click()
-  await page.waitForTimeout(5000)
-  expect(await page.locator('#gameList').innerText().toBe(''))
-})
-
-test('button returns to draw screen', async ({ page }) => {
-  await page.goto('http://localhost:4000/')
-  await page.getByRole('button', { name: 'Sign In' }).click()
-  await page.getByLabel('Username:').click()
-  await page.getByLabel('Username:').fill('rudi')
-  await page.getByLabel('Username:').press('Tab')
-  await page.getByLabel('Password:').fill('me')
-  await page
-    .locator('#loginForm')
-    .getByRole('button', { name: 'Login' })
-    .click()
-  await page.getByRole('button', { name: 'History' }).click()
-  await page.getByRole('button', { name: 'Back to Home' }).click()
-  expect(await page.getByRole('button', { name: 'History' }).toBeVisible())
+  await page.getByRole('button', { name: 'Previous' }).click()
+  expect(await page.getByText('test1 - 4/30/').isVisible())
 })
