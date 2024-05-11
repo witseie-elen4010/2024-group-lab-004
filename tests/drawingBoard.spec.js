d880dcf3e0f69ce1132e3fa47affb543b342819c
BrentTB <btbutkow@gmail.com>
2024-05-11 19:36:18 +0200

    remove previous test code addition

--- tests/drawingBoard.spec.js
+++ tests/drawingBoard.spec.js
@@ -6,15 +6,6 @@ async function navigateToGame(context) {
   await page1.getByRole('button', { name: 'Continue as Guest' }).click()
   await page1.getByPlaceholder('Enter your nickname').fill('test name 1')
   await page1.getByRole('button', { name: 'Join' }).click()
-  let signInButton = await page1.getByRole('button', {
-    name: 'Continue as Guest',
-    timeout: 500,
-  })
-  while (signInButton && (await signInButton.isVisible())) {
-    await signInButton.click()
-    await page1.getByPlaceholder('Enter your nickname').fill('test name 1')
-    await page1.getByRole('button', { name: 'Join' }).click()
-  }
   await page1.getByRole('button', { name: 'Create Public Game' }).click()
 
   const page2 = await context.newPage()
@@ -23,15 +14,6 @@ async function navigateToGame(context) {
   await page2.getByPlaceholder('Enter your nickname').fill('test name 2')
   await page2.getByRole('button', { name: 'Join' }).click()
   await page2.getByRole('button', { name: 'Join Public Game' }).click()
-  signInButton = await page2.getByRole('button', {
-    name: 'Continue as Guest',
-    timeout: 500,
-  })
-  while (signInButton && (await signInButton.isVisible())) {
-    await signInButton.click()
-    await page2.getByPlaceholder('Enter your nickname').fill('test name 1')
-    await page2.getByRole('button', { name: 'Join' }).click()
-  }
   await page2.getByRole('button', { name: /^Join$/ }).click()
 
   const page3 = await context.newPage()
@@ -40,15 +22,6 @@ async function navigateToGame(context) {
   await page3.getByPlaceholder('Enter your nickname').fill('test name 3')
   await page3.getByRole('button', { name: 'Join' }).click()
   await page3.getByRole('button', { name: 'Join Public Game' }).click()
-  signInButton = await page3.getByRole('button', {
-    name: 'Continue as Guest',
-    timeout: 500,
-  })
-  while (signInButton && (await signInButton.isVisible())) {
-    await signInButton.click()
-    await page3.getByPlaceholder('Enter your nickname').fill('test name 1')
-    await page3.getByRole('button', { name: 'Join' }).click()
-  }
   await page3.getByRole('button', { name: /^Join$/ }).click()
 
   await page1.getByRole('button', { name: 'Start Game' }).click()
