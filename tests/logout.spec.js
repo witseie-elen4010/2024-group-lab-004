fa4bb2e4470fa3c565c456f7229a09b23a239358
BrentTB <btbutkow@gmail.com>
2024-05-12 18:48:01 +0200

    welcome.js and logout.js tests re-added

--- tests/logout.spec.js
+++ tests/logout.spec.js
@@ -1,25 +1,36 @@
 import { test, expect } from '@playwright/test'
-test('random true test to pass', async ({ page }) => {
-  expect(true).toBe(true)
+
+test('logout button destroys session', async ({ context }) => {
+  const page1 = await context.newPage()
+  await page1.goto('http://localhost:4000/')
+  await page1.getByRole('button', { name: 'Continue as Guest' }).click()
+  await page1.getByPlaceholder('Enter your nickname').fill('test name 1')
+  await page1.getByRole('button', { name: 'Join' }).click()
+  await page1.getByRole('button', { name: 'Create Public Game' }).click()
+
+  const page2 = await context.newPage()
+  await page2.goto('http://localhost:4000/')
+  await page2.getByRole('button', { name: 'Continue as Guest' }).click()
+  await page2.getByPlaceholder('Enter your nickname').fill('test name 2')
+  await page2.getByRole('button', { name: 'Join' }).click()
+  await page2.getByRole('button', { name: 'Join Public Game' }).click()
+  await page2.getByRole('button', { name: /^Join$/ }).click()
+
+  const page3 = await context.newPage()
+  await page3.goto('http://localhost:4000/')
+  await page3.getByRole('button', { name: 'Continue as Guest' }).click()
+  await page3.getByPlaceholder('Enter your nickname').fill('test name 3')
+  await page3.getByRole('button', { name: 'Join' }).click()
+  await page3.getByRole('button', { name: 'Join Public Game' }).click()
+  await page3.getByRole('button', { name: /^Join$/ }).click()
+
+  await page1.getByRole('button', { name: 'Start Game' }).click()
+
+  await page1.getByRole('button', { name: 'Logout' }).click()
+  await page1.getByRole('button', { name: 'Sign In' }).click()
+  expect(
+    await page1
+      .getByRole('heading', { name: 'Login to Your Account' })
+      .isVisible()
+  )
 })
-// test('logout button destroys session', async ({ page }) => {
-//   await page.goto('http://localhost:4000')
-//   await page.getByRole('button', { name: 'Sign In' }).click()
-//   await page.getByLabel('Username:').click()
-//   await page.getByLabel('Username:').fill('test')
-//   await page.getByLabel('Password:').click()
-//   await page.getByLabel('Password:').fill('test')
-//   await page
-//     .locator('#loginForm')
-//     .getByRole('button', { name: 'Login' })
-//     .click()
-//   await page.getByRole('button', { name: 'History' }).click()
-//   await page.getByRole('button', { name: 'Back to Home' }).click()
-//   await page.getByRole('button', { name: 'Logout' }).click()
-//   await page.getByRole('button', { name: 'Sign In' }).click()
-//   expect(
-//     await page
-//       .getByRole('heading', { name: 'Login to Your Account' })
-//       .isVisible()
-//   )
-// })
