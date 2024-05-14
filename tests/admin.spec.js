b8ff1f02354a0bb2170ead22ee311dee13700a0d
Rudolph Torlage <torlagerudi@gmail.com>
2024-05-14 23:34:22 +0200

    edit admin test to assign button to variable

--- tests/admin.spec.js
+++ tests/admin.spec.js
@@ -6,26 +6,11 @@ test('admin hsitory', async ({ page }) => {
   await page.getByLabel('Username:').click()
   await page.getByLabel('Username:').fill('admin')
   await page.getByLabel('Password:').click()
-  await page.getByLabel('Password:').fill('3l3n4010')
-  await page
-    .locator('#loginForm')
-    .getByRole('button', { name: 'Login' })
-    .click()
-  await page.waitForLoadState('networkidle')
-  if (await page.getByRole('button', { name: 'Logout' }).isVisible()) {
-    await page.getByRole('button', { name: 'Logout' }).click()
-  }
-
-  await page.goto('http://localhost:4000/')
-  await page.getByRole('button', { name: 'Sign In' }).click()
-  await page.getByLabel('Username:').click()
-  await page.getByLabel('Username:').fill('admin')
-  await page.getByLabel('Password:').click()
   await page.getByLabel('Password:').fill(`3l3n4010`)
-  await page
+  const loginButton = await page
     .locator('#loginForm')
     .getByRole('button', { name: 'Login' })
-    .click()
+  await loginButton.click()
   await page.waitForLoadState('networkidle')
   await page.getByRole('button', { name: 'View History' }).click()
   await page.locator('li').first().click()
