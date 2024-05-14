17d5e6a8a56a6755cc4f3c6bb35d4c02b2609135
Rudolph Torlage <torlagerudi@gmail.com>
2024-05-14 20:25:14 +0200

    removed unused button

--- src/public/js/admin.js
+++ src/public/js/admin.js
@@ -2,7 +2,3 @@
 document.getElementById('viewHistory').addEventListener('click', () => {
   window.location.href = '/history'
 })
-
-document.getElementById('viewCurrentRooms').addEventListener('click', () => {
-  window.location.href = '/current-rooms'
-})
