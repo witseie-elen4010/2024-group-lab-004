e91a781d55c9db26efb75a677e97ad5e0c188376
Samuel Cohen <sam.cohen2001@gmail.com>
2024-05-15 09:39:43 +0200

    Retrying to set the room correctly

--- tests/landing.spec.js
+++ tests/landing.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-async function signIn(page) {
+async function signIn (page) {
   await page.goto('http://localhost:4000/')
   await page.getByRole('button', { name: 'Continue as Guest' }).click()
   await page.getByPlaceholder('Enter your nickname').fill('test name 1')
@@ -77,6 +77,7 @@ test.describe('Landing page tests', () => {
     await page.click('#joinRoom')
 
     // Check if the text area appears
+    await page.waitForSelector('#roomToJoin')
     const roomToJoinInput = await page.$('#roomToJoin')
     expect(roomToJoinInput).toBeTruthy()
     const submitJoinRoomButton = await page.$('#submitJoinRoom')
@@ -150,7 +151,7 @@ test.describe('Landing page tests', () => {
   })
 
   test('private room form is not visible after pressing join public room button', async ({
-    page,
+    page
   }) => {
     // Click on the join private room button
     await page.click('#joinRoom')
@@ -165,7 +166,7 @@ test.describe('Landing page tests', () => {
   })
 
   test('createPublicRoom button, when pressed, removes buttons and shows room ID with members: 1', async ({
-    context,
+    context
   }) => {
     // Go to landing page
     const page = await context.newPage()
