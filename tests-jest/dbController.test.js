dff8e3d983b7f68fb3a849e5385d01946e612e9b
BrentTB <btbutkow@gmail.com>
2024-05-16 17:48:31 +0200

    jest tests for dbController implemented

---
+++ tests-jest/dbController.test.js
@@ -0,0 +1,106 @@
+const { TextEncoder, TextDecoder } = require('util')
+global.TextEncoder = TextEncoder
+global.TextDecoder = TextDecoder
+
+const httpMocks = require('node-mocks-http')
+const db = require('../src/db/database')
+const {
+  fetchGames,
+  saveGrid,
+  fetchGrid,
+  newGame,
+} = require('../src/controller/dbController')
+
+jest.mock('../src/db/database')
+
+describe('fetchGames', () => {
+  it('should fetch games and return a JSON response', async () => {
+    const req = httpMocks.createRequest({
+      session: {
+        user: {
+          username: 'test',
+          id: 1,
+        },
+      },
+    })
+    const res = httpMocks.createResponse()
+
+    const mockGames = [{ game_id: 1 }, { game_id: 2 }]
+    db.query.mockResolvedValue({ rowCount: 2, rows: mockGames })
+
+    await fetchGames(req, res)
+
+    expect(db.query).toHaveBeenCalledWith(
+      'SELECT * FROM games WHERE user1 = $1 OR user2 = $1 OR user3 = $1 OR user4 = $1 OR user5 = $1 OR user6 = $1 OR user7 = $1 OR user8 = $1 ORDER BY game_id DESC',
+      [req.session.user.id]
+    )
+    expect(res._getJSONData()).toEqual(mockGames)
+  })
+})
+
+describe('saveGrid', () => {
+  it('should save a grid', async () => {
+    const gameID = 1
+    const grid = [
+      [0, 0],
+      [1, 1],
+    ]
+
+    db.query.mockResolvedValue({})
+
+    await saveGrid(gameID, grid)
+
+    expect(db.query).toHaveBeenLastCalledWith(
+      'INSERT INTO grids (grid, game_id) VALUES ($1, $2)',
+      [grid, gameID],
+      expect.any(Function)
+    )
+  })
+})
+
+describe('fetchGrid', () => {
+  it('should fetch a grid and return a JSON response', async () => {
+    const req = httpMocks.createRequest({
+      query: {
+        gameID: 1,
+      },
+    })
+    const res = httpMocks.createResponse()
+
+    const mockGrid = [
+      [0, 0],
+      [1, 1],
+    ]
+    db.query.mockResolvedValue({ rows: mockGrid })
+
+    await fetchGrid(req, res)
+
+    expect(db.query).toHaveBeenCalledWith(
+      'SELECT * FROM grids WHERE game_id = $1',
+      [req.query.gameID]
+    )
+    expect(res._getJSONData()).toEqual(mockGrid)
+  })
+})
+describe('newGame', () => {
+  it('should create a new game and return the game ID', (done) => {
+    const names = ['test1', 'test2', 'test3']
+    const host = 'test1'
+
+    const mockGameID = 1
+    db.query.mockImplementation((query, values, callback) => {
+      callback(null, { rows: [{ game_id: mockGameID }] })
+    })
+
+    newGame(names, host)
+      .then((gameID) => {
+        expect(db.query).toHaveBeenCalled()
+        expect(gameID).toEqual(mockGameID)
+        done() // Call done when the promise resolves
+      })
+      .catch((error) => {
+        console.error(error)
+        done() // Call done even if the promise rejects to avoid a timeout
+      })
+  })
+})
